1973435aaa51d0c6f002fa92cb6833a9c0e01947
Wanchopen <wanchopen@gmail.com>
2013-05-01 17:49:06 +0400

    First homework is ready, please check

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+function multiplyArray() {
+    var arrayResult = [];
+    for (n = 0; n < arguments.length; n++){
+        var array = arguments[n];
+        var result = array[0];
+        for (i = 1; i < array.length; i++) {
+            result = result * array[i];
+        }
+        arrayResult.push(result);
+    }
+    return arrayResult;
+
+}
+
+console.log(multiplyArray([2,3,5],[4,5]));
