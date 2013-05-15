50dc8aee34b67708e45cea6b6445fec88b337272
Andrey Balakirev <balakirev.andrey@gmail.com>
2013-05-15 18:03:21 +0400

    tests for hw4

--- homework4/homework4_test.js
+++ homework4/homework4_test.js
@@ -1,6 +1,11 @@
-var initialArray = [[3, 4, 5], [1, 5], [6, 8]],
-    finalArray = [];
-
-countArrays(initialArray);
-
-console.assert(finalArray === [60, 5, 48], 'wrong calculation');
+var finalArray = multiplyArray(
+    [
+        [3, 4, 5],
+        [1, 5],
+        [6, 8]
+    ]
+);
+console.assert(finalArray.length === 3, 'Wrong array length');
+console.assert(finalArray[0] === 60, 'Wrong first item');
+console.assert(finalArray[1] === 5, 'Wrong first item');
+console.assert(finalArray[2] === 48, 'Wrong first item');
