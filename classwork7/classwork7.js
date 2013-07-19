f2e10d9ff4e767510fa2a62aa8ad0a3b599eca22
Wanchopen <wanchopen@gmail.com>
2013-07-19 08:48:05 +0400

    Classwork about bind

---
+++ classwork7/classwork7.js
@@ -0,0 +1,19 @@
+Function.prototype.bind = function(context, arg){
+    return function(context){
+        context = this;
+    };
+};
+
+var a = function(arg1, arg2){
+    return this.prop + arg1 + arg2;
+};
+
+var thing = {
+    prop: 10
+};
+
+var b = a.bind(thing, 5);
+
+b(6);
+
+
