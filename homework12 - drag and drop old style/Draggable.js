b9088181a9b7c24ca239509d2c3522f5dda5d7ea
romanzagumennov <wanchopen@gmail.com>
2013-08-25 23:04:01 +0400

    drag and drop - corrected mistakes but failed to change style while onmousemove event is on

--- homework12 - drag and drop old style/Draggable.js
+++ homework12 - drag and drop old style/Draggable.js
@@ -1,37 +1,30 @@
 function Draggable(element){
-    this.el = element;
-    this.el.addEventListener("mousedown", this.onMouseDown.bind(this));
-    this.startElementPosition = {
+    this.onMouseMoveHandler = this.onMouseMove.bind(this);
+    this.onMouseUpHandler = this.onMouseUp.bind(this);
+    this._el = element;
+    this._el.addEventListener("mousedown", this.onMouseDown.bind(this));
+    this._startElementPosition = {
         x: 0,
         y: 0
     };
-    this.startMousePosition = {
+    this._startMousePosition = {
             x: 0,
             y: 0
         };
 }
 
 Draggable.prototype.onMouseDown = function(event){
-    document.body.addEventListener("mousemove", this.onMouseMove.bind(this));
-    document.body.addEventListener("mouseup", this.onMouseUp.bind(this));
+    document.body.addEventListener("mousemove", this.onMouseMoveHandler);
+    document.body.addEventListener("mouseup", this.onMouseUpHandler);
 
-    var elementStyleLeft = this.el.style.left;
+    var elementStyleLeft = this._el.style.left,
+        elementStyleTop = this._el.style.top;
 
-    var styleLeft = function(){
-        return parseInt(elementStyleLeft);
-    };
+    this._startElementPosition.x = parseInt(elementStyleLeft);
+    this._startElementPosition.y = parseInt(elementStyleTop);
 
-    var elementStyleTop = this.el.style.top;
-
-    var styleTop = function(){
-        return parseInt(elementStyleTop);
-    };
-
-    this.startElementPosition.x =  styleLeft();
-    this.startElementPosition.y =  styleTop();
-
-    this.startMousePosition.x = event.pageX;
-    this.startMousePosition.y = event.pageY;
+    this._startMousePosition.x = event.pageX;
+    this._startMousePosition.y = event.pageY;
 };
 
 Draggable.prototype.onMouseMove = function(event){
@@ -40,11 +33,13 @@ Draggable.prototype.onMouseMove = function(event){
         y: event.pageY
     };
 
-    this.el.style.top = this.startElementPosition.y + currentMousePosition.y - this.startMousePosition.y + 'px';
-    this.el.style.left = this.startElementPosition.x + currentMousePosition.x - this.startMousePosition.x + 'px';
+    this._el.style.top = this._startElementPosition.y + currentMousePosition.y - this._startMousePosition.y + 'px';
+    this._el.style.left = this._startElementPosition.x + currentMousePosition.x - this._startMousePosition.x + 'px';
+    this._el.style.borderColor = '#6A5ACD';
+    this._el.style.borderWidth = 3 + 'px';
 };
 
 Draggable.prototype.onMouseUp = function(){
-    document.body.removeEventListener("mousemove", this.onMouseMove.bind(this));
-    document.body.removeEventListener("mouseup", this.onMouseUp.bind(this));
+    document.body.removeEventListener("mousemove", this.onMouseMoveHandler);
+    document.body.removeEventListener("mouseup", this.onMouseUpHandler);
 };
